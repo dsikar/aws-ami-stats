8c74509c86fd937daaee4fbfa9650c5f14b76bfd
Daniel Sikar <dsikar@gmail.com>
2016-03-09 01:06:33 +0000

    Simplified parsing.

--- parser.pl
+++ parser.pl
@@ -2,9 +2,7 @@
 
 use strict;
 
-# set login
-my $dbuser = "root";
-my $dbpass = "1qaz2wsx";
+# set login in ~/.my.cnf
 
 my $file = $ARGV[0];
 my $region = substr $file, 0, index $file, ".";  
@@ -13,22 +11,9 @@ while(<FILE>)
 {
 	chomp; # remove end of line character
 	my @list = split('\t', $_);   
-	my $query = makeSQL(\@list);
-	sqlExec($query);
-}
-
-sub makeSQL
-{
-	my @list = @{$_[0]};
-	my $query = "INSERT INTO aws-public-ami.AMI(Architecture,CreationDate,Hypervisor,ImageId,ImageType,Name,OwnerId,RootDeviceType,VirtualizationType,ProductCode,Region)\n";
-	$query .= "VALUES (\"$list[0]\", \"$list[1]\", \"$list[2]\", \"$list[3]\", \"$list[4]\", \"$list[5]\", \"$list[6]\", \"$list[7]\", \"$list[8]\", \"$list[9]\", \"$region\")\n";
-	return $query;
-}
-
-sub sqlExec
-{
-	my($query) = @_;
-	print "mysql -u $dbuser -p$dbpass -e " . $query;
+	my $query = "uspInsertImage (" . "\"$list[0]\"" . ", " . "\"$list[1]\"" . "\"$list[2]\"" . ", " . "\"$list[3]\"" . ", " . "\"$list[4]\"" . ", " . "\"$list[5]\"" . ", ";
+	$query .= "\"$list[6]\"" . ", " . "\"$list[7]\"" . ", " . "\"$list[8]\"" . ", " . "\"none\"" . ", " . "\"$region\")";
+	# mysql insert query:  mysql -h "localhost" "aws_images" -e "$query"
+	print "mysql -e $query";
 	die;
 }
-
