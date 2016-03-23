15f5a3c6af372a3e836359d1c4adb5416266f3cb
Daniel Sikar <dsikar@gmail.com>
2016-03-23 19:05:26 +0000

    Platform by year query.

--- sql/queries/platform_by_year.sql
+++ sql/queries/platform_by_year.sql
@@ -6,3 +6,36 @@ SELECT
 	WHERE IMAGETYPE_ID = 2 
 	GROUP BY PLATFORM_ID, YEAR(CREATIONDATE) 
 	ORDER BY 2;
+/*
+
+TODO
+1. Change format
+	1.1 Headers for 1st year: YEAR, Linux (%), Windows(%), Total 
+	1.2 For every following year: YEAR, Linux (% +-%), Windows(% +-%), Total (+-%)
+
+2. Investigate Missing dates
+
++-------------+--------------------+----------+
+| PLATFORM_ID | YEAR(CREATIONDATE) | Count(*) |
++-------------+--------------------+----------+
+|           1 |                  0 |     2205 |
+|           2 |                  0 |       15 |
+|           1 |               2009 |      174 |
+|           2 |               2009 |        8 |
+|           1 |               2010 |     2539 |
+|           2 |               2010 |       32 |
+|           2 |               2011 |       13 |
+|           1 |               2011 |     4628 |
+|           2 |               2012 |       76 |
+|           1 |               2012 |    10756 |
+|           1 |               2013 |    14564 |
+|           2 |               2013 |      179 |
+|           2 |               2014 |      293 |
+|           1 |               2014 |    54220 |
+|           1 |               2015 |   185953 |
+|           2 |               2015 |     1609 |
+|           1 |               2016 |    55582 |
+|           2 |               2016 |      451 |
++-------------+--------------------+----------+
+
+*/
